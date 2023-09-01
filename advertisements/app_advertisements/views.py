7575c3c929a2149a71a4ec22fa4c3e5d65234f3e
mmmary7 <mariy7an@gmail.com>
2023-09-01 23:19:34 +0400

    'initial'

--- advertisements/app_advertisements/views.py
+++ advertisements/app_advertisements/views.py
@@ -2,5 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def index(request):
-    return HttpResponse("Успешно!")
+    return render(request, 'index.html')
 # Create your views here.
+def top_sellers(request):
+    return render(request, 'top-sellers.html')
