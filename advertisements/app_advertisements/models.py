8d52f22e3303fabc339d5515e56803a4cebebecd
mmmary7 <mariy7an@gmail.com>
2023-09-06 00:17:37 +0400

    'init'

--- advertisements/app_advertisements/models.py
+++ advertisements/app_advertisements/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.contrib import admin
 class Advertisement(models.Model):
     title = models.CharField("заголовок", max_length=128)
     description = models.TextField("описание")
@@ -12,3 +12,21 @@ class Advertisement(models.Model):
 
     class Meta:
         db_table = "advertisements"
+
+    @admin.display(description='дата создания')
+    def created_date(self):
+        from django.utils import timezone
+        from django.utils.html import format_html
+        if self.created_at.date() == timezone.now().date():
+            created_time = self.created_at.time().strftime("%H:%M:%S")
+            return format_html('<span style="color: green; font-weight: bold;">Сегодня в {}</span>', created_time)
+        return self.created_at.strftime("%d.%m.%Y в %H:%M:%S")
+
+    @admin.display(description='дата обновления')
+    def update_date(self):
+        from django.utils import timezone
+        from django.utils.html import format_html
+        if self.update_at.date() == timezone.now().date():
+            update_time = self.update_at.time().strftime("%H:%M:%S")
+            return format_html('<span style="color: purple; font-weight: bold;">Сегодня в {}</span>', update_time)
+        return self.update_at.strftime("%d.%m.%Y в %H:%M:%S")
